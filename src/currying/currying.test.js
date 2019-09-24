43e48ce6d105f6739a3993de525306f622467533
Roger Melo <rogerw592@gmail.com>
2019-09-24 09:16:35 -0300

    Writing Currying Tests

--- src/currying/currying.test.js
+++ src/currying/currying.test.js
@@ -1,5 +1,5 @@
-const add = () => {
-  return 3
+const add = (num1, num2) => {
+  return num1 + num2
 }
 
 test('add should be a function', () => {
@@ -9,6 +9,27 @@ test('add should be a function', () => {
 test('add(1, 2) should return 3', () => {
   const actual = add(1, 2)
   const expected = 3
+
+  expect(actual).toBe(expected)
+})
+
+test('add(3, 4) should return 7', () => {
+  const actual = add(3, 4)
+  const expected = 7
+
+  expect(actual).toBe(expected)
+})
+
+test('add(3, 4) should return 7', () => {
+  const actual = add(3, 4)
+  const expected = 7
+
+  expect(actual).toBe(expected)
+})
+
+test('add(5, 6) should return 7', () => {
+  const actual = add(3, 4)
+  const expected = 7
 
   expect(actual).toBe(expected)
 })
