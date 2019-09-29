9e8977f6ceb614beda19683b7a150ad98a016c56
Roger Melo <rogerw592@gmail.com>
2019-09-29 18:57:32 -0300

    Add Unhappy Path Tests

--- src/01-the-longest-word/the-longest-word.js
+++ src/01-the-longest-word/the-longest-word.js
@@ -1,6 +1,13 @@
-const getLongestWord = (aString) =>
-  aString
-    .split(/[\s,:;.?!()"']/gm)
-    .reduce((acc, curr) => acc.length >= curr.length ? acc : curr, '')
+const getLongestWord = (aString) => {
+  if (typeof aString !== 'string') {
+    throw new TypeError('getLongestWord should receive a string as argument')
+  }
+
+  return !/\s/g.test(aString)
+    ? 'The passed string should have two or more words'
+    : aString
+        .split(/[\s,:;.?!()"']/gm)
+        .reduce((acc, curr) => acc.length >= curr.length ? acc : curr, '')
+}
 
 export default getLongestWord
--- src/01-the-longest-word/the-longest-word.test.js
+++ src/01-the-longest-word/the-longest-word.test.js
@@ -1,24 +1,70 @@
 import getLongestWord from './the-longest-word'
 
-const dancingPhrase = 'É proibido dançar agarrado mas se quiser, pode'
-const mathPhrase = '22 alunos entre os dez primeiros colocados'
+describe('Happy Path', () => {
+  const dancingPhrase = 'É proibido dançar agarrado mas se quiser, pode'
+  const mathPhrase = '22 alunos entre os dez primeiros colocados'
 
-test('getLongestWord should be a function', () => {
-  expect(getLongestWord).toBeInstanceOf(Function)
+  test('getLongestWord should be a function', () => {
+    expect(getLongestWord).toBeInstanceOf(Function)
+  })
+
+  test(`getLongestWord(${dancingPhrase}) should return "proibido"`, () => {
+      const actual = getLongestWord(dancingPhrase)
+      const expected = 'proibido'
+
+      expect(actual).toBe(expected)
+    }
+  )
+
+  test(`getLongestWord(${mathPhrase}) should return "primeiros"`, () => {
+      const actual = getLongestWord(mathPhrase)
+      const expected = 'primeiros'
+
+      expect(actual).toBe(expected)
+    }
+  )
 })
 
-test(`getLongestWord(${dancingPhrase}) should return "proibido"`, () => {
-    const actual = getLongestWord(dancingPhrase)
-    const expected = 'proibido'
+describe('Unhappy Path', () => {
+  test('getLongestWord() should thrown an TypeError', () => {
+    try {
+      getLongestWord()
+    } catch (error) {
+      expect(error.name).toBe('TypeError')
+    }
+  })
+
+  const typeErrorMessage = 'getLongestWord should receive a string as argument'
+
+  test(`getLongestWord() should thrown an TypeError with message "${typeErrorMessage}"`, () => {
+    try {
+      getLongestWord()
+    } catch (error) {
+      expect(error.message).toBe(typeErrorMessage)
+    }
+  })
+
+  test(`getLongestWord(7) should thrown an TypeError with message "${typeErrorMessage}"`, () => {
+    try {
+      getLongestWord(7)
+    } catch (error) {
+      expect(error.message).toBe(typeErrorMessage)
+    }
+  })
+
+  const noSpaceMessage = 'The passed string should have two or more words'
+
+  test(`getLongestWord('oiCaraDeBoi') should return a message "${noSpaceMessage}"`, () => {
+    const actual = getLongestWord('oiCaraDeBoi')
+    const expected = noSpaceMessage
 
     expect(actual).toBe(expected)
-  }
-)
+  })
 
-test(`getLongestWord(${mathPhrase}) should return "primeiros"`, () => {
-    const actual = getLongestWord(mathPhrase)
-    const expected = 'primeiros'
+  test(`getLongestWord('Twitter') should return a message "${noSpaceMessage}"`, () => {
+    const actual = getLongestWord('Twitter')
+    const expected = noSpaceMessage
 
     expect(actual).toBe(expected)
-  }
-)
+  })
+})
